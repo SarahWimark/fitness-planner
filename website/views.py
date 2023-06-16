fed20001695008bd667b298cb1fbba8359b19dde
SarahWimark <sarahwimark@gmail.com>
2023-06-16 11:49:36 +0200

    Adding html pages and routes for home and calendar page

--- website/views.py
+++ website/views.py
@@ -5,4 +5,9 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('home.html')
+
+
+@views.route('/calendar')
+def calendar():
+    return render_template('calendar.html')
