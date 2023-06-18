cee6b86f19e6c1b1007a777d6c2621f45db6e315
SarahWimark <sarahwimark@gmail.com>
2023-06-18 13:32:20 +0200

    Working on calendar page

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
+import calendar
 
 
 def create_app():
     app = Flask(__name__)
-
     from .views import views
     app.register_blueprint(views, url_prefix='/')
     return app
--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from calendar import monthcalendar
 
 views = Blueprint('views', __name__)
 
@@ -10,4 +11,7 @@ def home():
 
 @views.route('/calendar')
 def calendar():
-    return render_template('calendar.html')
+    june = monthcalendar(2023, 6)
+    #print(calendar)
+    print(june[2][2])
+    return render_template('calendar.html', june=june)
