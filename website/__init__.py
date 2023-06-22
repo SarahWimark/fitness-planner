e4f689e09bdedfa7f9899713a2b952039b331424
SarahWimark <sarahwimark@gmail.com>
2023-06-22 13:59:14 +0200

    Adding config info

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+import os
 
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.urandom(24)
     from .views import views
     app.register_blueprint(views, url_prefix='/')
     return app
