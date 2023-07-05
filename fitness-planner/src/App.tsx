8bdc9d213a8b04070f3a0682badeee4e1baf1ad4
SarahWimark <sarahwimark@gmail.com>
2023-07-05 20:41:18 +0200

    Working on acitivty component

--- fitness-planner/src/App.tsx
+++ fitness-planner/src/App.tsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import Calendar from "./components/calendar/Calendar";
+import Activities from "./components/activity/Activities";
 
 const App = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
 
   return (
     <>
-      <div className="mt-16 flex flex-col items-center">
-        <Calendar currentDate={currentDate} onChange={setCurrentDate} />
+      <div className="container mx-auto flex flex-row">
+        <div className="mt-16 items-center">
+          <Calendar currentDate={currentDate} onChange={setCurrentDate} />
+        </div>
+
+        <Activities />
       </div>
     </>
   );
--- fitness-planner/src/components/activity/Activities.tsx
+++ fitness-planner/src/components/activity/Activities.tsx
@@ -1,5 +1,14 @@
 const Activities = () => {
-  return <div>Activities</div>;
+  return (
+    <div className="border p-4">
+      <h2 className="text-base font-bold">Planned activities</h2>
+      <ul>
+        <li>Planned acitivity 1</li>
+        <li>Planned acitivity 2</li>
+        <li>Planned acitivity 3</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Activities;
